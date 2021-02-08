f53f80c3b8b5e0c84372ebe74a6d1e2a6db4ddd9
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-09 01:40:42 +0530

    Refactered: printed the length of line1 and line2.

--- lineComparisonProb.java
+++ lineComparisonProb.java
@@ -32,6 +32,7 @@ class LineComparisonProb{
 			
 		length1 = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
 		length2 = Math.sqrt(Math.pow((x4 - x3),2) +  Math.pow((y4 - y3),2));	
+   	System.out.println("\n Length of Line1="+length1+" and Line2="+length2);
 
       if(length1==length2)
 			System.out.println("\n The lines are Equal. ");
