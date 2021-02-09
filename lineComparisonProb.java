8f116d6bd06f5435104d11f36eb04b419129392b
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-02-10 01:59:00 +0530

    Update lineComparisonProb.java

--- lineComparisonProb.java
+++ lineComparisonProb.java
@@ -3,20 +3,20 @@ class LineComparisonProb{
    public static void main(String[] args){
       float x1,y1,x2,y2;
       double length;
-		System.out.println("Welcome to Line Comparison Problems.");
+      System.out.println("Welcome to Line Comparison Problems.");
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("\n Enter x1-y1 co-ordinates of the line.");
-		x1 = sc.nextFloat();
-     	y1 = sc.nextFloat();
+      Scanner sc = new Scanner(System.in);
+      System.out.println("\n Enter x1-y1 co-ordinates of the line.");
+      x1 = sc.nextFloat();
+      y1 = sc.nextFloat();
 
-		System.out.println("\n Enter x2-y2 co-ordinates of the line.");
+      System.out.println("\n Enter x2-y2 co-ordinates of the line.");
       x2 = sc.nextFloat();
       y2 = sc.nextFloat();
 
       System.out.println("\n Coordinates of the line are x1="+x1+ " y1="+y1+" and x2="+x2 + " y2="+y2);
 
-		length = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
-		System.out.println("\nLength of the line : "+ length);
-	}
+      length = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
+      System.out.println("\nLength of the line : "+ length);
+   }
 }
