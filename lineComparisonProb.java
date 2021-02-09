6e87bb25c66d5e7ccabd7b322a199928f4fffd97
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-10 02:03:16 +0530

    Refactererd: Found line1 equal to line2 or not.

--- lineComparisonProb.java
+++ lineComparisonProb.java
@@ -6,25 +6,21 @@ class LineComparisonProb{
 		System.out.println("Welcome to Line Comparison Problems.");
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("\n Enter x1 co-ordinate of the line.");
-		x1 = sc.nextFloat();
-      System.out.println("\n Enter y1 co-ordinate of the line.");
+      System.out.println("\n Enter x1-y1 co-ordinates of the line.");
+      x1 = sc.nextFloat();
       y1 = sc.nextFloat();
 
-		System.out.println("\n Enter x2 co-ordinate of the line.");
+      System.out.println("\n Enter x2-y2 co-ordinates of the line.");
       x2 = sc.nextFloat();
-      System.out.println("\n Enter y2 co-ordinate of the line.");
       y2 = sc.nextFloat();
 
-		System.out.println("\n Enter x3 co-ordinate of the line.");
-      x3 = sc.nextFloat();
-      System.out.println("\n Enter y3 co-ordinate of the line.");
-      y3 = sc.nextFloat();
+      System.out.println("\n Enter x3-y3 co-ordinates of the line.");
+      x1 = sc.nextFloat();
+      y1 = sc.nextFloat();
 
-		System.out.println("\n Enter x4 co-ordinate of the line.");
-      x4 = sc.nextFloat();
-      System.out.println("\n Enter y4 co-ordinate of the line.");
-      y4 = sc.nextFloat();
+      System.out.println("\n Enter x24-y4 co-ordinates of the line.");
+      x2 = sc.nextFloat();
+      y2 = sc.nextFloat();
       
 		System.out.println("\n Coordinates of the line1 is x1="+x1+ " y1="+y1+" and x2="+x2 + " y2="+y2);
  		System.out.println("\n Coordinates of the line2 is x3="+x3+ " y3="+y3+" and x4="+x4 + " y4="+y4);
@@ -33,7 +29,7 @@ class LineComparisonProb{
 		length1 = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
 		length2 = Math.sqrt(Math.pow((x4 - x3),2) +  Math.pow((y4 - y3),2));	
 
-      if(length1==length2)
+      if(length1 == length2)
 			System.out.println("\n The lines are Equal. ");
 		else
          System.out.println("\n The lines are not Equal. ");
