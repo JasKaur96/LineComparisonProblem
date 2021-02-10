d0a92cae841a8713d478b0d790b2c085b7194062
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-02-10 19:33:52 +0530

    Update lineComparisonProb.java

--- lineComparisonProb.java
+++ lineComparisonProb.java
@@ -4,7 +4,6 @@ class LineComparisonProb{
    	float x1,y1,x2,y2,x3,y3,x4,y4;
 	double length1,length2;
 	System.out.println("Welcome to Line Comparison Problems.");
-
 	Scanner sc = new Scanner(System.in);
 	System.out.println("\n Enter x1-y1 co-ordinates of the line.");
         x1 = sc.nextFloat();
@@ -24,13 +23,10 @@ class LineComparisonProb{
             
 	System.out.println("\n Coordinates of the line1 is x1="+x1+ " y1="+y1+" and x2="+x2 + " y2="+y2);
  	System.out.println("\n Coordinates of the line2 is x3="+x3+ " y3="+y3+" and x4="+x4 + " y4="+y4);
-
 	length1 = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
 	length2 = Math.sqrt(Math.pow((x4 - x3),2) +  Math.pow((y4 - y3),2));	
    	System.out.println("\n Length of Line1="+length1+" and Line2="+length2);
-
 	compareTo(length1,length2);
-
    }
    public static void compareTo(double length1,double length2){	
 	if(length1 == length2)
