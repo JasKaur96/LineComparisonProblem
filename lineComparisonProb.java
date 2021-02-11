42ef5a02afd7c9c44e63fdcf96e340b9d7ddb585
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-11 19:05:46 +0530

    Refactered: Used Oops concept to compare lines.

--- lineComparisonProb.java
+++ lineComparisonProb.java
@@ -1,41 +1,39 @@
 import java.util.Scanner;
 class LineComparisonProb{
+   private final double x1;
+   private final double y1;
+   private final double x2;
+   private final double y2;
+
+   public LineComparisonProb(double x1, double y1, double x2, double y2){
+      this.x1 = x1;
+      this.y1 = y1;
+      this.x2 = x2;
+      this.y2 = y2;
+   }
+
+   public double lengthOfLine(){
+      double length = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
+      return length;
+   }
+
+   public static void compareTo(double length1,double length2){
+      System.out.println("\n Length of line1=" + length1 +  "\n Length of line2="+length2);
+      if(length1 == length2)
+            System.out.println("\n The lines are Equal. ");
+      else if (length1 > length2)
+            System.out.println("\n Line1 is greater than line2. ");
+      else
+            System.out.println("\n Line2 is greater than Line1. ");
+   }
+
    public static void main(String[] args){
-		System.out.println("Welcome to Line Comparison Problems.");
-		float x1 = 0, y1 = 0, x2 = 0 ,y2 = 0, x3 = 0, y3 = 0,x4 = 0 ,y4 = 0;
-  	   double length1,length2;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("\n Enter x1-y1 co-ordinates of the line.");
-      x1 = sc.nextFloat();
-      y1 = sc.nextFloat();
-
-      System.out.println("\n Enter x2-y2 co-ordinates of the line.");
-      x2 = sc.nextFloat();
-      y2 = sc.nextFloat();
-
-      System.out.println("\n Enter x3-y3 co-ordinates of the line.");
-      x3 = sc.nextFloat();
-      y3 = sc.nextFloat();
-
-      System.out.println("\n Enter x24-y4 co-ordinates of the line.");
-      x4 = sc.nextFloat();
-      y4 = sc.nextFloat();
-            
-		System.out.println("\n Coordinates of the line1 is x1="+x1+ " y1="+y1+" and x2="+x2 + " y2="+y2);
- 		System.out.println("\n Coordinates of the line2 is x3="+x3+ " y3="+y3+" and x4="+x4 + " y4="+y4);
-
-      length1 = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
-		length2 = Math.sqrt(Math.pow((x4 - x3),2) +  Math.pow((y4 - y3),2));	
-   	System.out.println("\n Length of Line1="+length1+" and Line2="+length2);
-		compareTo(length1,length2);
-	}
- 	public static void compareTo(double length1,double length2){	
-		if(length1 == length2)
-         	System.out.println("\n The lines are Equal. ");
-     	else if (length1 > length2)
-         	System.out.println("\n line1 is grater than line2. ");
-     	else
-      	System.out.println("\n Line2 is grater than Line1. ");
-	}
-	
+      System.out.println("******Welcome to Line Comparison Problems.******");
+      LineComparisonProb line1 = new LineComparisonProb(2.3,3.5,5.3,8.5);
+      LineComparisonProb line2 = new LineComparisonProb(3.3,4.5,5.3,5.5);
+      double length1 = line1.lengthOfLine();
+      double length2 = line2.lengthOfLine();
+      compareTo(length1,length2);
+   }
 }
+
